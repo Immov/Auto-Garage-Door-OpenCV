5ee08e794b95db022d80fb24e0ab51b9d9f320c9
Immov <immanuel.giovano@gmail.com>
2022-12-12 20:48:21 +0700

    code improvement

--- Yolov-car-vid.py
+++ Yolov-car-vid.py
@@ -26,74 +26,78 @@ def get_output_layers(net):
         output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
     return output_layers
 
-def yolov3car(gambar, conf=0.5):
-	# Load Yolo
-	net = cv2.dnn.readNet(yolo_weights, yolo_cfg) # get from https://pjreddie.com/darknet/yolo/
-	# Name custom object
-	classes = ["car"]
-	# Create blob
-	blob = cv2.dnn.blobFromImage(gambar, 1/255, (416, 416), (0, 0, 0), True, crop=False)
-	Width = gambar.shape[1]
-	Height = gambar.shape[0]
-	# Set the input the the net
-	net.setInput(blob)
-	# Get the output layers
-	outs = net.forward(get_output_layers(net))
-	# Initialize list of detected bounding boxes
-	bounding_boxes = []
-	# Loop over the layers and get the detected bounding boxes
-	for out in outs:
-		for detection in out:
-			# Get the score
-			scores = detection[5:]
-			class_id = np.argmax(scores)
-			confidence = scores[class_id]
-			# Get the bounding box
-			if confidence > conf:
-				# Rescale the bounding box
-				center_x = int(detection[0] * Width)
-				center_y = int(detection[1] * Height)
-				w = int(detection[2] * Width)
-				h = int(detection[3] * Height)
-				# Add to list of bounding boxes
-				x = int(center_x - w / 2)
-				y = int(center_y - h / 2)
-				bounding_boxes.append([x, y, w, h])
-				# Draw rectangle
-				cv2.rectangle(gambar, (x, y), (x + w, y + h), (0, 255, 0), 2)
-	return gambar
-
 def rescaleFrame(frame, scale=0.75):
 	width = int(frame.shape[1] * scale)
 	height = int(frame.shape[0] * scale)
 	dimensions = (width, height)
-
 	return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
+
+def detect_cars(image):
+	# load our yolov7 model
+	net = cv2.dnn.readNet("yolov7-tiny.weights", "yolov7-tiny.cfg")
+	# get the image height, width and shape
+	height, width, channels = image.shape
+	# create a blob from the image
+	blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+	# set the input to the network
+	net.setInput(blob)
+	# run the forward pass
+	outs = net.forward(get_output_layers(net))
+	# initialize the list of detected cars
+	detected_cars = []
+	# loop over the detections
+	for out in outs:
+		# loop over each detection
+		for detection in out:
+			# get the score and class
+			scores = detection[5:]
+			class_id = np.argmax(scores)
+			# ignore everything except cars
+			if class_id == 2: #2 == cars
+				# get the confidence and center coordinates
+				confidence = float(scores[class_id])
+				# x = int(detection[0] * width)
+				# y = int(detection[1] * height)
+				# w = int(detection[2] * width)
+				# h = int(detection[3] * height)
+				center_x = int(detection[0] * width)
+				center_y = int(detection[1] * height)
+				w = int(detection[2] * width)
+				h = int(detection[3] * height)
+				# Add to list of bounding boxes
+				x = int(center_x - w / 2)
+				y = int(center_y - h / 2)
+
+				detected_cars.append((x, y, w, h))
+	# draw the bounding boxes on the image
+	i = 0
+	for car in detected_cars:
+		i+=1
+		x, y, w, h = car
+		cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 4)
+	if(i!=0):
+		print(f"{i} Car feature is detected!")
 
 # cap = cv2.VideoCapture(0) # Webcam
 # cap = cv2.VideoCapture('Videos/Small/Forza.mp4') # File
 cap = cv2.VideoCapture('Videos/car.mp4') # File
-# cap = cv2.VideoCapture('http://192.168.1.51:4747/Video') # Network
+# cap = cv2.VideoCapture('http://192.168.1.51:4747/video') # Network
 
-# FPS Limiter
+
 fps = 30
-target_fps = 5
-fps_timer = fps/target_fps
-start_frame_number = 0
 
-
+framecount = 0
 while True:
-	cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame_number)
 	success, frame = cap.read()
-	# frame = rescaleFrame(frame, scale=.4)
-	frame = yolov3car(frame)
-	cv2.imshow('Video', frame)
+	frame = rescaleFrame(frame, .5)
+	if(framecount%(fps/1)==0): #Detecting every 1 second
+		print("Detecting...")
+		detect_cars(frame)
+	cv2.imshow('Video',frame)
 	if cv2.waitKey(20) & 0xFF==ord('d'): # if d is pressed, then break
 		break
-	start_frame_number+=fps_timer
-
+	framecount+=1
 
 cap.release()
 cv2.destroyAllWindows()
-
 cv2.waitKey(0)
