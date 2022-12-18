84c36e1ea96e1a263bb49405c2d2b43fdd198920
Immov <immanuel.giovano@gmail.com>
2022-12-18 22:56:16 +0700

    multi view detect fix timing

--- Yolov-car-vid.py
+++ Yolov-car-vid.py
@@ -9,8 +9,8 @@ def rescaleFrame(frame, scale=0.75):
 	return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
 
 # cap = cv2.VideoCapture(0) # Webcam
-# cap = cv2.VideoCapture('Videos/Small/Forza.mp4') # File
-cap = cv2.VideoCapture('Videos/car.mp4') # File
+cap = cv2.VideoCapture('Videos/Record/lands1.mp4') # File
+# cap = cv2.VideoCapture('Videos/car.mp4') # File
 # cap = cv2.VideoCapture('http://192.168.1.51:4747/video') # Network
 
 fps = 30
@@ -19,14 +19,13 @@ while True:
 	success, frame = cap.read()
 	frame = rescaleFrame(frame, .5)
 	if(framecount%(fps/1)==0): #Detecting every 1 second
-		print("Detecting...")
-		yolo.detect_cars(frame, framecount)
-		cv2.imwrite("Original.jpg", frame)
+		detect = yolo.detect_cars(frame, framecount)
+		cv2.imshow('Detect', detect)
 	cv2.imshow('Video',frame)
 	if cv2.waitKey(20) & 0xFF==ord('d'): # if d is pressed, then break
 		break
 	framecount+=1
-	break
+
 
 cap.release()
 cv2.destroyAllWindows()
--- yolo.py
+++ yolo.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import time
 
 # # Yolo v3-tiny
 # yolo_weights = "yolov3-tiny.weights"
@@ -17,6 +18,25 @@ import numpy as np
 yolo_weights = "yolov7-tiny.weights"
 yolo_cfg = "yolov7-tiny.cfg"
 
+# Removes duplicated boxes
+def merge_boxes(boxes, treshold):
+    # Initialize empty list for merged boxes
+    merged_boxes = []
+    # Sort boxes by x-coordinate
+    boxes.sort(key=lambda x: x[0])
+    # Iterate over boxes and try to merge them
+    while len(boxes) > 0:
+        box = boxes.pop(0)
+        merged = False
+        for i, mbox in enumerate(merged_boxes):
+            if abs(box[0] - mbox[0]) < treshold and abs(box[1] - mbox[1]) < treshold:
+                # Merge boxes
+                merged_boxes[i] = (min(box[0], mbox[0]), min(box[1], mbox[1]), max(box[2], mbox[2]), max(box[3], mbox[3]))
+                merged = True
+                break
+        if not merged:
+            merged_boxes.append(box)
+    return merged_boxes
 
 
 def get_output_layers(net):
@@ -27,34 +47,29 @@ def get_output_layers(net):
 		output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 	return output_layers
 
-def detect_cars(image, frame_num):
-	# load our yolov7 model
+def detect_cars(img, frame_num):
+	image = img.copy()
 	net = cv2.dnn.readNet("yolov7-tiny.weights", "yolov7-tiny.cfg")
-	# get the image height, width and shape
 	height, width, channels = image.shape
-	# create a blob from the image
 	blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-	# set the input to the network
 	net.setInput(blob)
-	# run the forward pass
 	outs = net.forward(get_output_layers(net))
-	# initialize the list of detected cars
 	detected_cars = []
 	# loop over the detections
+	start_time = time.time()
 	for out in outs:
 		# loop over each detection
 		for detection in out:
 			# get the score and class
 			scores = detection[5:]
 			class_id = np.argmax(scores)
-			# ignore everything except cars
 			if class_id == 2: #2 == cars
 				# get the confidence and center coordinates
 				confidence = float(scores[class_id])
-				x = int(detection[0] * width)
-				y = int(detection[1] * height)
-				w = int(detection[2] * width)
-				h = int(detection[3] * height)
+				# x = int(detection[0] * width)
+				# y = int(detection[1] * height)
+				# w = int(detection[2] * width)
+				# h = int(detection[3] * height)
 
 				center_x = int(detection[0] * width)
 				center_y = int(detection[1] * height)
@@ -65,15 +80,24 @@ def detect_cars(image, frame_num):
 				y = int(center_y - h / 2)
 
 				detected_cars.append((x, y, w, h))
+	end_time = time.time()
 
-	print(detected_cars)
+	elapsed_time = end_time - start_time
+	print(f'Yolo detecting for {elapsed_time:.6f} seconds')
+	detected_cars = merge_boxes(detected_cars, 10)
 
-	# Export each cars
+	# Every 5 seconds
 	i=0
-	for car in detected_cars:
-		x, y, w, h = car
-		cv2.imwrite(f'Images/Result/{frame_num}-CAR-{i}-X{x}.Y{y}-.jpg', image[y:y+h, x:x+w])
-		i+=1
+	if(frame_num%150==0):
+		# Export each cars
+		for car in detected_cars:
+			x, y, w, h = car
+			if(x<0):
+				x=0
+			if(y<0):
+				y=0
+			# cv2.imwrite(f'Images/Result/FRAME{frame_num}-CAR{i}.jpg', image[y:y+h, x:x+w])
+			i+=1
 
 	# draw the bounding boxes on the image
 	i = 0
@@ -85,3 +109,4 @@ def detect_cars(image, frame_num):
 		print(f"{i} Car feature is detected!")
 	else:
 		print("No car is detected")
+	return image
