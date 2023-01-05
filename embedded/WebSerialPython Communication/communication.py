6097ee2bfa111ecf5fde49d439ad64090d84c29f
Immanuel Giovano <immanuel.giovano@gmail.com>
2023-01-05 18:56:21 +0700

    gitMerge remote-tracking branch 'refs/remotes/origin/main'

--- embedded/WebSerialPython Communication/communication.py
+++
@@ -1,3 +0,0 @@
-import requests
-r = requests.post('http://192.168.4.1:80/set_data', data="{'number':55}") # remember that you get the keyword 'number' in the server side
-print(r.text) # should print "Some message"
